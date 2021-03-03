bf3e2a7cdb481a3048af889d9d56ae21105bacbb
Freddy10035 <Freddy10035@users.noreply.github.com>
2021-03-03 11:30:21 +0300

    First Commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy;Online Library Management System |<a href="https://itsourcecode.com/" target="_blank"  > Designed by : ITSOURCECODE</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
